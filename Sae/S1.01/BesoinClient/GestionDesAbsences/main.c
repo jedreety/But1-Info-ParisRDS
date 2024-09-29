ded7ed3b2c0cbd4ece9960a9285d1d326bc358f1
jedreety <130364840+jedreety@users.noreply.github.com>
2024-09-29 13:45:32 +0200

    1 - sae 1.01

--- Sae/S1.01/BesoinClient/GestionDesAbsences/main.c
+++ Sae/S1.01/BesoinClient/GestionDesAbsences/main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <string.h>
+#pragma warning(disable:4996)
+
+enum {LONGUEUR_COMMANDE = 100};
 
 int main() {
-	printf("Hello, World!\n");
-	return 0;
+
+	char commande[LONGUEUR_COMMANDE];
+
+	printf("Bienvenue sur l'application de gestion des absences. \n\n");
+	
+	while (1)
+	{
+		printf("Entrer une commade : ");
+
+		// Lecture de la commande
+		if (scanf("%s", commande) != 1)
+		{
+			printf("Erreur lors de la lecture de la commande.\n");
+			return 1;
+		}
+		else
+		{
+			commande[LONGUEUR_COMMANDE - 1] = '\0';
+		}
+
+		// Traitement de la commande
+		if (strcmp(commande, "exit") == 0)
+		{
+			printf("Au revoir !\n");
+			break;
+		}
+		else
+		{
+			printf("Commande inconnue.\n");
+		}
+		
+	}
+
 }
