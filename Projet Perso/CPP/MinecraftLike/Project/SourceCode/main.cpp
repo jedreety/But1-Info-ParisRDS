f0bbe7a278d013c803179c8c62bd6150dc7173d2
jedreety <130364840+jedreety@users.noreply.github.com>
2024-10-16 20:00:34 +0200

    Debut du projet !

---
+++ Projet Perso/CPP/MinecraftLike/Project/SourceCode/main.cpp
@@ -0,0 +1,23 @@
+/*
+	Projet Minecraft Clone
+	Auteur : Mehdi Mazouz
+	Date : 2024
+
+	Ce projet a pour but de créer une engine en C++ ainsi qu'un jeu.
+
+	L'objectif de ce projet est de développer une engine qui soit le plus optimisé possible et expendable.
+	Je ne m'aiderais pas de chatgpt sauf pour setup vs sinon je n'utiliserais que des ressources en ligne et papier.
+
+	* Prediction *
+	Fun : 10/10 Vraiment 
+	Difficulté : 10/10 C'est long et dur
+	Utilité : 10/10 J'ai ennormement appris
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
